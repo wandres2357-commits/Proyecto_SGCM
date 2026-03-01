269635d8b13ad7d2bc98fd4acc114a181fe0e2ac
wandres2357-commits <w.andres2357@gmail.com>
2026-03-01 11:34:20 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import AuthWrapper from "./AuthWrapper";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthWrapper />
+  </React.StrictMode>
+);
